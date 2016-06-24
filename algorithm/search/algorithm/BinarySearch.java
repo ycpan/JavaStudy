6aea53a24ea30b9d2590b838b2bd43a71c99059e
Pan <Pan@DESKTOP-RA16KIR>
2016-06-24 20:41:44 +0800

    delete somethig

---
+++ algorithm/search/algorithm/BinarySearch.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+public class BinarySearch {
+	
+	public static void main(String[] args){
+		int[] li = { 1, 3, 5, 8, 10, 20};
+		System.out.println(binarySearch(li,2));
+	} 
+	
+	public static int binarySearch(int list[],int key){
+		int low = 0;
+		int high = list.length - 1;
+		
+		while(low <= high) {
+			int mid = (low + high) / 2;
+			if(key < list[mid])
+				high = mid -1;
+				
+			if(list[mid] == key)
+				return mid;
+			if(key > list[mid])
+				
+				low = mid + 1;
+			
+		}
+		return (high );
+		
+	}
+	
+
+}
